2a840bc07e131e47b75514d11093d0379157748a
nemanjaaks <nemanjaa@stud.ntnu.no>
2015-05-05 11:36:23 -0700

    --

--- app.js
+++ app.js
@@ -536,15 +536,15 @@ app.get('/facebook-account',ensureAuthenticatedFacebook, function(req, res){
 
 app.get('/facebook-videos', ensureAuthenticatedFacebook,function(req,res){
     console.log('INSIDE FB ACC'+req.user);
-    var query = models.fbUser.where({fb_id: req.user.fb_id});
+    var query = models.fbUser.where({fb_id: sess.fb_id});
     query.findOne(function(err,user){
         if(err) return handleError(err);
         if(user){
-            graph.setAccessToken(req.user.access_token);
+            graph.setAccessToken(user.access_token);
 
             graph.get('me/home?filter=app_2392950137',  function(err, response){
                 console.log(response);
-                res.render('facebook-videos', {user: req.user, url:response});
+                res.render('facebook-videos', {user: user, url:response});
             });
 
         }
